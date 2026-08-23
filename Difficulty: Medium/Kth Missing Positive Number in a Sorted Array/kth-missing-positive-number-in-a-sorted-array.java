be4705706ba4fcda271276daac14f6f5046a9464
ashifshekh393-coder <ashifshekh393@gmail.com>
2026-08-23 13:12:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int lo = 0, hi = arr.length -1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            int correctno = mid+1;
+            int missing = arr[mid] - correctno;
+            if(missing >=k) hi = mid-1;
+            else lo = mid+1;
+        }
+        return hi +1+k;
+    }
+}
